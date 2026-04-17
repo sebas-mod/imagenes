89f40d4ddd47d7aa00741921bc91e3e98b39e971
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-17 16:28:27 -0300

    Update certificado.js

--- api/certificado.js
+++ api/certificado.js
@@ -1,15 +1,17 @@
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { name1, name2 } = req.query
 
   if (!name1 || !name2) {
     return res.status(400).send('Faltan nombres')
   }
 
-  const text = encodeURIComponent(`${name1} ❤️ ${name2}`)
+  const base = 'https://res.cloudinary.com/dlg955drd/image/upload'
 
-  // 🔥 API que genera imagen con texto
-  const imageUrl = `https://api.popcat.xyz/drake?text1=${text}&text2=Certificado%20de%20Amor`
+  const url = `${base}/
+  l_text:Arial_60:${encodeURIComponent(name1)},co_rgb:d16b86,g_center,x_200,y_-20/
+  l_text:Arial_60:${encodeURIComponent(name2)},co_rgb:d16b86,g_center,x_200,y_80/
+  l_text:Arial_30:${encodeURIComponent(new Date().toLocaleDateString())},co_rgb:555555,g_south,y_40/
+  v1776453975/ChatGPT_Image_17_abr_2026_04_25_50_p.m._xdjcaa.png`
 
-  res.setHeader('Content-Type', 'application/json')
-  res.json({ url: imageUrl })
+  res.redirect(url.replace(/\s+/g, ''))
 }

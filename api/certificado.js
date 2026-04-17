947d65661c94bf53ff26117e7d59e1a75b0457f7
Sebas-MD <169330308+sebas-mod@users.noreply.github.com>
2026-04-17 16:35:05 -0300

    Update certificado.js

--- api/certificado.js
+++ api/certificado.js
@@ -9,10 +9,11 @@ export default function handler(req, res) {
 
   const fecha = new Date().toLocaleDateString('es-AR')
 
-  // 🔥 URL limpia (SIN espacios ni saltos)
-  const url = base +
-    '/l_text:Arial_60:' + encodeURIComponent(name1) + ',co_rgb:d16b86,g_center,x_200,y_-20' +
-    '/l_text:Arial_60:' + encodeURIComponent(name2) + ',co_rgb:d16b86,g_center,x_200,y_80' +
+  // 🔥 FORMATO CORRECTO (font_family_font_size:texto)
+  const url =
+    base +
+    '/l_text:Arial_50:' + encodeURIComponent(name1) + ',co_rgb:d16b86,g_center,x_200,y_-20' +
+    '/l_text:Arial_50:' + encodeURIComponent(name2) + ',co_rgb:d16b86,g_center,x_200,y_80' +
     '/l_text:Arial_30:' + encodeURIComponent(fecha) + ',co_rgb:555555,g_south,y_40' +
     '/v1776453975/ChatGPT_Image_17_abr_2026_04_25_50_p.m._xdjcaa.png'
 
